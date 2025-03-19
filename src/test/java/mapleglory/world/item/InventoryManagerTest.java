1f6e28b5f9a14ceb2a263a25f06f7e2114123425
Orr Goren <orrgorenn@gmail.com>
2025-03-19 23:40:02 +0200

    fix ut

--- src/test/java/mapleglory/world/item/InventoryManagerTest.java
+++ src/test/java/mapleglory/world/item/InventoryManagerTest.java
@@ -96,12 +96,12 @@ public final class InventoryManagerTest {
         Assertions.assertEquals(ORANGE_POTION, im.getConsumeInventory().getItem(2).getItemId());
         Assertions.assertEquals(5, im.getConsumeInventory().getItem(2).getQuantity());
 
-        Assertions.assertTrue(im.addItem(createItem(RED_POTION, 100)).isPresent());
-        Assertions.assertEquals(100, im.getConsumeInventory().getItem(1).getQuantity());
+        Assertions.assertTrue(im.addItem(createItem(RED_POTION, 200)).isPresent());
+        Assertions.assertEquals(200, im.getConsumeInventory().getItem(1).getQuantity());
         Assertions.assertNotNull(im.getConsumeInventory().getItem(3));
         Assertions.assertEquals(5, im.getConsumeInventory().getItem(3).getQuantity());
 
-        Assertions.assertFalse(im.addItem(createItem(RED_POTION, 100)).isPresent());
+        Assertions.assertFalse(im.addItem(createItem(RED_POTION, 200)).isPresent());
     }
 
     @Test
