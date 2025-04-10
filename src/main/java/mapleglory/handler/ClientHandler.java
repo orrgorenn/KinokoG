6563f83c68bf7a093ec82cb253101c38656b343a
Orr Goren <orrgorenn@gmail.com>
2025-04-11 01:01:21 +0300

    add custom packets to auto-login

--- src/main/java/mapleglory/handler/ClientHandler.java
+++ src/main/java/mapleglory/handler/ClientHandler.java
@@ -1,12 +1,19 @@
 package mapleglory.handler;
 
+import mapleglory.packet.CustomPacket;
 import mapleglory.server.header.InHeader;
 import mapleglory.server.header.OutHeader;
+import mapleglory.server.migration.MigrationInfo;
+import mapleglory.server.migration.TransferInfo;
+import mapleglory.server.node.ChannelInfo;
 import mapleglory.server.node.Client;
+import mapleglory.server.node.LoginServerNode;
 import mapleglory.server.packet.InPacket;
 import mapleglory.util.Util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
 
 public final class ClientHandler {
     private static final Logger log = LogManager.getLogger(ClientHandler.class);
@@ -30,5 +37,39 @@ public final class ClientHandler {
 
         final short op = inPacket.decodeShort();
         log.error("Error {} | {}({}) | {}", errorType, OutHeader.getByValue(op), Util.opToString(op), inPacket);
+    }
+
+    // Custom Packets
+
+    @Handler(InHeader.RegisterMigration)
+    public static void handleRegisterMigration(Client c, InPacket inPacket) {
+        final int accountId = inPacket.decodeInt();
+        final int characterId = inPacket.decodeInt();
+        final int channelId = inPacket.decodeInt();
+        final byte[] machineId = inPacket.decodeArray(16);
+        final byte[] clientKey = c.getClientKey();
+
+        // Resolve target channel
+        final LoginServerNode loginServerNode = (LoginServerNode) c.getServerNode();
+        final Optional<ChannelInfo> channelInfoResult = loginServerNode.getChannelById(channelId);
+        if (channelInfoResult.isEmpty()) {
+            log.error("Could not resolve target channel for migration request for character ID : {}", characterId);
+            c.write(CustomPacket.registerMigrationResultFail());
+            return;
+        }
+
+        // Create and submit migration request
+        final MigrationInfo migrationInfo = MigrationInfo.from(channelId, accountId, characterId, machineId, clientKey);
+        loginServerNode.submitLoginRequest(migrationInfo, (transferResult) -> {
+            if (transferResult.isEmpty()) {
+                log.error("Failed to submit migration request for character ID : {}", characterId);
+                c.write(CustomPacket.registerMigrationResultFail());
+                return;
+            }
+
+            // Send migration result success
+            final TransferInfo transferInfo = transferResult.get();
+            c.write(CustomPacket.registerMigrationResultSuccess(transferInfo.getChannelHost(), transferInfo.getChannelPort(), clientKey));
+        });
     }
 }
---
+++ src/main/java/mapleglory/packet/CustomPacket.java
@@ -0,0 +1,21 @@
+package mapleglory.packet;
+
+import mapleglory.server.header.OutHeader;
+import mapleglory.server.packet.OutPacket;
+
+public final class CustomPacket {
+    public static OutPacket registerMigrationResultSuccess(byte[] channelHost, int channelPort, byte[] clientKey) {
+        final OutPacket outPacket = OutPacket.of(OutHeader.RegisterMigrationResult);
+        outPacket.encodeByte(0);
+        outPacket.encodeArray(channelHost);
+        outPacket.encodeShort(channelPort);
+        outPacket.encodeArray(clientKey);
+        return outPacket;
+    }
+
+    public static OutPacket registerMigrationResultFail() {
+        final OutPacket outPacket = OutPacket.of(OutHeader.RegisterMigrationResult);
+        outPacket.encodeByte(1);
+        return outPacket;
+    }
+}
